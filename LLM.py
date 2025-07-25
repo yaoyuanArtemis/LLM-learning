43f4196cf363d6753f73be7fae560ee895b0bac1
yaoyuanArtemis <liufeng0194@gmail.com>
2025-07-25 14:05:19 +0800

    add:split train_set and validation_set

--- LLM.py
+++ LLM.py
@@ -6,7 +6,7 @@ import requests  # 2.32.4
 
 
 
-# 如果没有文件 请求数据集
+# 请求数据集
 if not os.path.exists("sales-textbooks.txt"):
     url = "https://huggingface.co/datasets/goendalf666/sales-textbook_for_convincing_and_selling/resolve/main/sales_textbook.txt?download=true"
     with open("sales-textbooks.txt","wb") as f:
@@ -16,8 +16,15 @@ with open("sales-textbooks.txt","r") as f:
     text = f.read()
     # print("🚀 text:",text)
 
+
+# tokenize化
 import tiktoken
 encoding = tiktoken.get_encoding("o200k_base")
 tokenized_text = encoding.encode(text)
-print("tokenized_text: ",tokenized_text)
-print(len(tokenized_text))
+# print("tokenized_text: ",tokenized_text)
+
+
+# split train_set and validation_set
+train_index = int(len(tokenized_text) * 0.9)
+train_data = tokenized_text[:train_index]
+validate_data = tokenized_text[train_index:]
