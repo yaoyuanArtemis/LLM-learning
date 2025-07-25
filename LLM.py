77efea26fe93b2ba1a76d79440fc8e3c166aaf02
yaoyuanArtemis <liufeng0194@gmail.com>
2025-07-25 15:10:35 +0800

    add:create Input Embedding Table

--- LLM.py
+++ LLM.py
@@ -21,6 +21,7 @@ with open("sales-textbooks.txt","r") as f:
 import tiktoken
 encoding = tiktoken.get_encoding("o200k_base")
 tokenized_text = encoding.encode(text)
+tokenized_text = torch.tensor(tokenized_text,dtype=torch.long)
 # print("tokenized_text: ",tokenized_text)
 
 
@@ -33,3 +34,23 @@ validate_data = tokenized_text[train_index:]
 context_length = 16 
 d_modal = 64
 bach_size = 4
+
+# 训练数据中整批次整理好数据
+data = train_data
+idxs = torch.randint(low=0,high=len(data) - context_length,size=(bach_size,))
+x_batch = torch.stack([data[idx:idx + context_length] for idx in idxs])
+y_batch = torch.stack([data[idx+1:idx + context_length+1] for idx in idxs])
+print(x_batch)
+
+import pandas as pd
+# print(pd.DataFrame(x_batch[0].numpy()))
+
+# create Input Embedding Table
+max_token_value = tokenized_text.max().item()
+input_embedding_lookup_table = nn.Embedding(max_token_value+1,d_modal)
+print("input_embedding_lookup_table",input_embedding_lookup_table)
+x_batch_embedding = input_embedding_lookup_table(x_batch)
+y_batch_embedding = input_embedding_lookup_table(y_batch)
+
+# positional encoding
+
