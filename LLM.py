fa3da8af1ea441915dca3a984274cfa9e1c2bf0e
yaoyuanArtemis <liufeng0194@gmail.com>
2025-07-25 13:40:57 +0800

    add:新增获取数据集以及输出到文件中

--- LLM.py
+++ LLM.py
@@ -1,5 +1,17 @@
-import torch
+import torch     # 2.7.1
 import torch.nn as nn
-import torch.nn.Functional as F
+import torch.nn.functional as F
 import os
-import request
+import requests  # 2.32.4
+
+
+
+# 如果没有文件 请求数据集
+if not os.path.exists("sales-textbooks.txt"):
+    url = "https://huggingface.co/datasets/goendalf666/sales-textbook_for_convincing_and_selling/resolve/main/sales_textbook.txt?download=true"
+    with open("sales-textbooks.txt","wb") as f:
+        f.write(requests.get(url).content)
+
+with open("sales-textbooks.txt","r") as f:
+    text = f.read()
+    print("🚀 text:",text)
